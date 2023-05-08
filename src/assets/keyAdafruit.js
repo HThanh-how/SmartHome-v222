78a2487bea60dc9c44936f72f2fff031d970192b
HThanh-how <thanhlt1235@gmail.com>
2023-05-08 21:44:57 +0700

    test key

--- src/assets/keyAdafruit.js
+++ src/assets/keyAdafruit.js
@@ -1,4 +1,4 @@
-const str = "FCQRBe9JjjjDrUOS5Dpueo57amOa_oia";
+const str = "AvQCA4zJUZiJ374jvn9fgP31FCCc_oia";
 const key = str.split("").reverse().join("");
-// const key="aio_aOma75oeupD5SOUrDjjjJ9eBRQCF";
+
 export default key;
